40bae0ba9a557d0cb115eaa027fad27ce1710657
sourabh.zanwar <sourabh.zanwar@reth-aachen.de>
2021-07-15 12:50:36 +0200

    fix

--- push.js
+++ push.js
@@ -9,6 +9,15 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {};
+let sub = {
+    endpoint:
+        "https://fcm.googleapis.com/fcm/send/eCas2llpD-I:APA91bHMCt6XlNFOUVmgWm9buohDv-_zbTlZqV33cm_Es1WoYZV-3ViPrsGeKraVcxH442wK9qafN1IlPuaIP5ocFRQPjNaa2z3W6z92WVX_LdLRzpkIzMJMqzihKFrrdmSkIqIC85Vd",
+    expirationTime:null,
+    keys:
+        {
+            p256dh:"BI_z7I4GwiZCyxGdkEzMEEnAWWvc4jQ2LpNk4pFqSmBKR0_KYUGsDFlfQQpJ7VkTml77hsdFtWFfDHIwhE0whsQ",
+            auth:"ePK4EjzHFP3fxNgbPsfq_g"
+        }
+    };
 
 push.sendNotification(sub, 'text message')
